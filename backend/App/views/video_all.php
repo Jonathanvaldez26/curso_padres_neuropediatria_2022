b9fb276830a4f670c47d5679be6efd6617447f0b
Your Name <jonathan.valdez@grupolahe.com>
2022-05-14 09:35:21 -0500

    Hola

--- backend/App/views/video_all.php
+++ backend/App/views/video_all.php
@@ -141,19 +141,9 @@
         <div class="modal fade" id="encuesta" role="dialog" aria-labelledby="encuestaLabel" aria-hidden="true">
             <div class="modal-dialog modal-size" role="document">
                 <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="encuestaLabel">Examen para <?php echo $nombre_taller;?></h5>
-                        <button type="button" class="btn bg-gradient-danger text-lg btn-icon-only" data-dismiss="modal" aria-label="Close">
-                            <span class="" aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
                     <form id="encuesta_curso" action="" method="post">
                         <div class="modal-body">
                             aqui mete el video
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" id="enviar_encuesta" class="btn bg-gradient-success">Enviar</button>
-                            <button type="button" class="btn bg-gradient-secondary" data-dismiss="modal">Cancelar</button>
                         </div>
                     </form>
                 </div>
